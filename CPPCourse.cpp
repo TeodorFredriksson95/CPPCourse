3e138b5723eba9a50243cdf59eb3532a6ae78e9f
teodor.fredriksson <teo95@hotmail.se>
2026-02-26 09:46:57 +0100

    Trying to learn folder and file organization in c++ project. Pushing test folder.

--- CPPCourse.cpp
+++ CPPCourse.cpp
@@ -1,7 +1,9 @@
 #include "GuessANumber.h"
+#include "Test/Test.h"
 #include <iostream>
 
 int main()
 {
+    TestFunc();
     Week1::GuessANumber();
 }
---
+++ Test/Test.cpp
@@ -0,0 +1,6 @@
+#include "Test.h"
+#include <iostream>
+
+void TestFunc() {
+	std::cout << "writing from test file";
+}
