61f61a3a19994342b4b9d173ce223ca76db23888
TeodorFredriksson95 <teo95@hotmail.se>
2026-03-13 17:30:21 +0100

    Practiced classes and constructors

--- src/Exercises/Week2/W2_Level1.cpp
+++ src/Exercises/Week2/W2_Level1.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 namespace Week2::Level1 {
+    // Template function for FUNSIES!!
     void RunTemplateFunction() {
         cout << MaxNumber<int>(1, 2) << endl;
     }
@@ -17,15 +18,44 @@ namespace Week2::Level1 {
         void Increment() {
             value++;
         }
+
         void Reset() {
             value = 0;
         }
+
         constexpr int GetNumber() const {
             return value;
         }
     };
 
+    struct Vec2 {
+        int x;
+        int y;
+    };
 
     // 5. Class Player: private Vec2 pos. Constructor sets start pos. Methods MoveUp/Down/Left/Right and Pos()
     // const.
+    class Player {
+    private:
+        Vec2 pos;
+
+    public:
+        Player(const Vec2 pos)
+            : pos {pos}
+        {
+        }
+
+        void MoveUp(const Vec2& currentPos) {
+            pos.y = currentPos.y - 1;
+        }
+        void MoveDown(const Vec2& currentPos) {
+            pos.y = currentPos.y + 1;
+        }
+        void MoveLeft(const Vec2& currentPos) {
+            pos.x = currentPos.x - 1;
+        }
+        void MoveRight(const Vec2& currentPos) {
+            pos.x = currentPos.x + 1;
+        }
+    };
 }
